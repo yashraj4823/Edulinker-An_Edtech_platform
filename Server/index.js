4d9153594682786db66b85c7868854a15250dfe5
Roushan shah <108416766+Roushanshah@users.noreply.github.com>
2024-06-19 00:30:32 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"study-notion-an-edtech-platform-five.vercel.app
+",
         credentials:true,
     })
 );
