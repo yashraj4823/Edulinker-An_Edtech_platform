25a89e475553113c0ce2e067ec87c0d79bf68777
Roushan shah <108416766+Roushanshah@users.noreply.github.com>
2024-06-22 19:20:34 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"study-notion-an-edtech-platform-ten.vercel.app",
+        origin:"https://study-notion-an-edtech-platform-ten.vercel.app",
         credentials:true,
     })
 );
